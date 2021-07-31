2233327054d926fd3954c480e1bf2938e24ca617
Cikappa2904 <cikappa.0429@gmail.com>
2021-07-31 22:17:22 +0200

    Starting project
    Created BrowserChooser, that taken a command line argument opens the corresponding OpenWith dialog

---
+++ BrowserChooser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BrowserChooser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string argument = " Shell32.dll,OpenAs_RunDLL "  + args[0] ;
+            Console.WriteLine(argument);
+            Process.Start("Rundll32.exe", argument);
+        }
+    }
+}
