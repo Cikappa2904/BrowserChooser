d913f1f2fc6074e563627fc0c2f9b2e3600adc1b
Cikappa2904 <cikappa.0429@gmail.com>
2021-08-01 16:25:22 +0200

    Finished installer (UI sucks but it works so...)

--- Installer/Form1.Designer.cs
+++ Installer/Form1.Designer.cs
@@ -31,56 +31,60 @@ namespace Installer
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(225, 97);
+            this.textBox1.Location = new System.Drawing.Point(12, 120);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(334, 20);
+            this.textBox1.Size = new System.Drawing.Size(265, 20);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "C:\\Program Files";
+            this.textBox1.Text = "C:\\Program Files\\BrowserChooser";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(357, 136);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(117, 189);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(139, 53);
             this.button1.TabIndex = 1;
             this.button1.Text = "Install";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 259);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(565, 95);
+            this.button2.Location = new System.Drawing.Point(293, 120);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 20);
             this.button2.TabIndex = 3;
             this.button2.Text = "Browse";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 326);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(356, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -92,8 +96,8 @@ namespace Installer
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- Installer/Form1.cs
+++ Installer/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
@@ -22,14 +23,20 @@ namespace Installer
         private void button1_Click(object sender, EventArgs e)
         {
             //Selecting where the file will be stored
-            string downloader = " https://raw.githubusercontent.com/Cikappa2904/BrowserChooser/main/BrowserChooser/bin/Debug/netcoreapp3.1/BrowserChooser.exe" + " -o \"" + textBox1.Text + "\\BrowserChooser.exe" + "\"";
+            string downloader = " https://raw.githubusercontent.com/Cikappa2904/BrowserChooser/main/BrowserChooser/bin/Release/netcoreapp3.1/publish/BrowserChooser.exe" + " -o \"" + textBox1.Text + "\\BrowserChooser.exe" + "\"";
             
+            if(!Directory.Exists(textBox1.Text))
+            {
+                System.IO.Directory.CreateDirectory(textBox1.Text);
+            }
+
             //Downloading the file
             Process downloadProcess = new Process();
             downloadProcess.StartInfo.FileName = "curl";
             downloadProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden; //Hides the cmd window
             downloadProcess.StartInfo.Arguments = downloader;
             downloadProcess.Start();
+            progressBar1.Value = 10;
 
             string filePath = "\"" + textBox1.Text + "\\BrowserChooser.exe" + "\" %1";
 
@@ -38,31 +45,45 @@ namespace Installer
             URL.SetValue("FriendlyTypeName", "Browser Chooser");
             URL.SetValue("URL Protocol", "");
             URL.Close();
+            progressBar1.Value = 20;
+
             RegistryKey Shell = Registry.ClassesRoot.CreateSubKey(@"BrowserChooserURL\shell\open\command");
             Shell.SetValue("", filePath);
             Shell.Close();
+            progressBar1.Value = 30;
+
             RegistryKey RegApps = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\RegisteredApplications", true);
             RegApps.SetValue("Browser Chooser", "Software\\Clients\\StartMenuInternet\\BrowserChooser\\Capabilities");
             RegApps.Close();
+            progressBar1.Value = 40;
+
             RegistryKey StartMenuInternet = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\\Clients\\StartMenuInternet\\BrowserChooser\\Capabilities");
             StartMenuInternet.SetValue("ApplicationDescription", "Choose what browser you want to use every time");
             string filePath2 = textBox1.Text + "\\BrowserChooser.exe" + ",0";
             StartMenuInternet.SetValue("ApplicationIcon", filePath2);
             StartMenuInternet.SetValue("ApplicationName", "Browser Chooser");
+            StartMenuInternet.Close();
+            progressBar1.Value = 50;
+
             RegistryKey StartMenuInternet2 = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\\Clients\\StartMenuInternet\\BrowserChooser\\Capabilities\\Startmenu");
             StartMenuInternet2.SetValue("StartMenuInternet", "Browser Chooser");
             StartMenuInternet2.Close();
+            progressBar1.Value = 60;
+
             RegistryKey StartMenuInternet3 = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\\Clients\\StartMenuInternet\\BrowserChooser\\Capabilities\\URLAssociations");
             StartMenuInternet3.SetValue("http", "BrowserChooserURL");
             StartMenuInternet3.SetValue("https", "BrowserChooserURL");
             StartMenuInternet3.Close();
+            progressBar1.Value = 70;
             RegistryKey StartMenuInternet4 = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\\Clients\\StartMenuInternet\\BrowserChooser\\DefaultIcon");
             StartMenuInternet4.SetValue("", filePath2);
             StartMenuInternet4.Close();
+            progressBar1.Value = 80;
             RegistryKey StartMenuInternet5 = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\\Clients\\StartMenuInternet\\BrowserChooser\\shell\\open\\command");
             string filePath3 = textBox1.Text + "\\BrowserChooser.exe";
             StartMenuInternet5.SetValue("", filePath3);
             StartMenuInternet5.Close();
+            progressBar1.Value = 100;
         }
 
         private void button2_Click(object sender, EventArgs e)
