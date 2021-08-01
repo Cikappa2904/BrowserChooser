0c345e638e27ebc6e0472d8ed43ebf1ddb8ccc71
Cikappa2904 <cikappa.0429@gmail.com>
2021-08-01 15:53:17 +0200

    Published BrowserChooser as single exe

--- BrowserChooser/obj/Debug/netcoreapp3.1/BrowserChooser.AssemblyInfo.cs
+++ BrowserChooser/obj/Debug/netcoreapp3.1/BrowserChooser.AssemblyInfo.cs
@@ -11,13 +11,15 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("BrowserChooser")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Cikappa2904")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Choose what browser you want every time you click on a link")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrowserChooser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrowserChooser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/Cikappa2904/BrowserChooser")]
 
 // Generato dalla classe WriteCodeFragment di MSBuild.
 
