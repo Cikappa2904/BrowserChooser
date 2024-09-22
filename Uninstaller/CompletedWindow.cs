99f95829df7d1a292dfb24edfc173c07490852a5
Cikappa2904 <49496033+Cikappa2904@users.noreply.github.com>
2024-09-22 15:59:38 +0200

    tried to fix uninstaller deletion WIP

--- Uninstaller/CompletedWindow.cs
+++ Uninstaller/CompletedWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32.TaskScheduler;
+using System;
 using System.Diagnostics;
 using System.Windows.Forms;
 
@@ -16,15 +17,34 @@ namespace Uninstaller
         {
             string fileName = Application.ExecutablePath.Substring(0, Application.ExecutablePath.LastIndexOf("\\"));
 
-            //TODO: make sure to delete also the folder
+            //TODO: delete the folder with a scheduled task
 
             Process.Start(new ProcessStartInfo()
             {
                 Arguments = "/C timeout /t 3 & rmdir /s /q \"" + fileName + "\"",
-                WindowStyle = ProcessWindowStyle.Hidden,
+                //WindowStyle = ProcessWindowStyle.Hidden,
                 CreateNoWindow = true,
-                FileName = "cmd.exe"
+                FileName = "cmd.exe",
+                UseShellExecute= true,
             });
+
+            // Create a new TaskService instance
+            using (TaskService ts = new TaskService())
+            {
+                // Create a new task definition and assign properties
+                TaskDefinition td = ts.NewTask();
+                td.RegistrationInfo.Description = "My Task that runs after 1 minute";
+
+                // Define a new action for the task (e.g., a console application or script)
+                td.Actions.Add(new ExecAction("cmd.exe", "-c rmdir /q /s \"" + fileName + "\"", null));
+
+                // Set a trigger to start the task 1 minute from now
+                td.Triggers.Add(new TimeTrigger { StartBoundary = DateTime.Now.AddSeconds(4) });
+
+                // Register the task in the root folder
+                ts.RootFolder.RegisterTaskDefinition("tempsask", td);
+            }
+
             Application.Exit();
         }
     }
